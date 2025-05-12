dc4f238a333036876a4dcba1235f00d7f3c3b750
Estes786 <elmatador0197@gmail.com>
2025-05-12 16:31:33 +0000

    Test simple homepage

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = __dirname;
+export default function HomePage() {
+      return (
+          <div style={{ padding: '50px', textAlign: 'center' }}>
+                <h1>Halo Dunia!</h1>
+                      <p>Tes Halaman Sederhana.</p>
+                          </div>
+                            );
+                            }
+                            
+}
